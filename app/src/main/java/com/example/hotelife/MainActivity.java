047c822a1220a278053fd04cfc05e3d4d0976e69
D4niel5 <danielnaniperez@gmail.com>
2021-10-06 21:19:48 -0500

    prueba borrada

--- app/src/main/java/com/example/hotelife/MainActivity.java
+++ app/src/main/java/com/example/hotelife/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//un com0nemt
+//un com0nemt  jjjh
     }
 }
