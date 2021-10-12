7e3dd589359d70d9d534653fe74fdb0527c71dc5
DavidRVN <riozv95@gmail.com>
2021-10-12 17:24:38 -0500

    Se crean la navegacion inicial para el registro y verificacion del administrador

--- app/src/main/java/com/example/hotelife/MainActivity.java
+++ app/src/main/java/com/example/hotelife/MainActivity.java
@@ -2,16 +2,27 @@ package com.example.hotelife;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register_admin);
+        setContentView(R.layout.activity_main);
 
-
+        Button btn_register = findViewById(R.id.btn_register);
+        btn_register.setOnClickListener(v->{
+            showRegisterAdmin();
+        });
 
     }
+
+    protected void showRegisterAdmin(){
+        Intent registerAdmin = new Intent(this,register_admin.class);
+        startActivity(registerAdmin);
+    }
+
 }
--- app/src/main/java/com/example/hotelife/register_admin.java
+++ app/src/main/java/com/example/hotelife/register_admin.java
@@ -2,7 +2,9 @@ package com.example.hotelife;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class register_admin extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class register_admin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_admin);
+
+        Button btn_continue = findViewById(R.id.btn_continue_register);
+        btn_continue.setOnClickListener(v->{
+            showVerification();
+        });
+
+    }
+
+    protected void showVerification(){
+        Intent verification = new Intent(this, verification.class);
+        startActivity(verification);
     }
 }
