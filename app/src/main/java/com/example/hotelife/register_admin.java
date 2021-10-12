5a746131534cd768dbafc848cc618c62484e4ac0
DavidRVN <riozv95@gmail.com>
2021-10-11 21:19:38 -0500

    Se crean los archivos de idiomas Se agregan varios colores se crea la interfaz de inicio y el registro de administrador

--- app/src/main/java/com/example/hotelife/register_admin.java
+++ app/src/main/java/com/example/hotelife/register_admin.java
@@ -4,14 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class register_admin extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_admin);
-
-
-
     }
 }
